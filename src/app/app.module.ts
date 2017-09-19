ce9449db39e79d6df51bab642eca630c83818335
Lalmansingh <l640395@heb.com>
2017-09-19 16:07:38 -0500

    Add banner inline component and tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BannerComponent } from './banner-inline/banner-inline.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    LoginComponent,
+    WelcomeComponent
   ],
   imports: [
     BrowserModule
