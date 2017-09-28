70e2a3233169e2ab2eae9263882b2369ddc54b82
Matthew <gpjazzplayer@gmail.com>
2017-09-27 19:06:02 -0500

    Added original AngularJS code to login component
    - Added logic to component
    - Added css

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,224 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  DUPLICATE_EMAIL_ERROR = 'That email is already in use. Please try again.';
+  DEFAULT_REGISTRATION_ERROR = 'There was an error registering as a new user. Please try again.';
+
+  // Error messages
+  loginError = undefined;
+  registerError = undefined;
+  forgotPasswordError = undefined;
+  forgotPasswordResponse = undefined;
+
+  // Form models
+  login = {};
+  register = {};
+  forgotPassword = {};
+
+  // Flags
+  isLogin = true;
+  isRegister = false;
+  isForgotPassword = false;
+  disableSubmit = false;
+  promoCheckbox = true;
+  recaptchaLoaded = false;
+
+  // Public methods available to the HTML
+  /* submitLogin = submitLogin;
+  submitRegister = submitRegister;
+  submitForgotPassword = submitForgotPassword;
+  toggleView = toggleView;
+  toggleOverlay = this.toggleOverlay;
+  toggleForgotPassword = toggleForgotPassword;
+  setWidgetId = setWidgetId; */
+
+  //test-code
+  // Methods not used in HTML but need testing
+  /* clearLoginForm = clearLoginForm;
+  clearRegisterForm = clearRegisterForm;
+  clearForgotPasswordForm = clearForgotPasswordForm; */
+  //end-test-code
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  /**
+   * Listener for overlay-close. On broadcast will clear both forms and
+   * default to the login form.
+   */
+  /* $scope.$on('login.close', function () {
+    clearLoginForm();
+    clearRegisterForm();
+    clearForgotPasswordForm();
+    this.isLogin = true;
+    this.isForgotPassword = false;
+  }); */
+
+   /* clearLoginForm() {
+    this.login = {};
+    this.loginError = undefined;
+    if (this.loginForm) {
+      this.login.username = '';
+      this.loginForm.$setPristine();
+      this.loginForm.$setUntouched();
+    }
+  }
+
+  clearRegisterForm() {
+    this.register = {};
+    this.registerError = undefined;
+  if (this.registerForm) {
+    this.register.emailAddress = '';
+    this.register.emailConfirm = '';
+    this.register.password = '';
+    this.register.passwordConfirm = '';
+    this.registerForm.$setPristine();
+    this.registerForm.$setUntouched();
+  }
+} */
+
+/**
+ * Clears the forgot password form AND
+ * resets the recaptcha.
+ */
+/* function clearForgotPasswordForm() {
+  login.forgotPassword = {};
+  login.forgotPasswordError = undefined;
+  if (login.forgotPassword) {
+    login.forgotPasswordResponse = undefined;
+    login.forgotPasswordForm.$setPristine();
+    login.forgotPasswordForm.$setUntouched();
+    if (login.recaptchaLoaded) {
+      vcRecaptchaService.reload(login.widgetId);
+    }
+  }
+} */
+
+/**
+ * Handles log in request and handles errors. Should only contain emailAddress and password
+ */
+/* function submitLogin() {
+  //login.loadingTracker.addPromise(login.promise);
+  login.disableSubmit = true;
+  // Invalidate current user
+  AuthenticationService.invalidate();
+  AuthenticationService.login(login.login.username, login.login.password, login.loadingTracker)
+    .then(loginSuccess)
+    .catch(loginFailure);
+
+  function loginSuccess() {
+    login.disableSubmit = false;
+    $rootScope.$broadcast(CART_OPERATIONS.changeItemCount);
+    $rootScope.$broadcast(AUTHENTICATION_OPERATIONS.login);
+    toggleOverlay();
+    clearLoginForm();
+  } */
+
+  /* function loginFailure(data) {
+    login.disableSubmit = false;
+    if (data.status == 401) {
+      login.loginError = data.data.message[0].message;
+    } else if (data.status == 403) {
+      login.loginError = 'User is not authorized to access this application';
+    } else if (data.status == 429) {
+      login.loginError = 'This account has been locked. After 24 hours your account will be automatically unlocked. For immediate access, reset your password by clicking the \"Forgot Password\" link below.'
+    } else {
+      login.loginError = 'An unexpected error has occurred';
+    }
+  }
+} */
+
+/**
+ * Handles user registration request and handles errors. Should contain full object.
+ */
+// function submitRegister() {
+//   login.disableSubmit = true;
+//   login.register.username = login.register.emailAddress;
+//   login.register.receiveEmail = login.promoCheckbox;
+//   CustomerService.register(login.register, login.loadingTracker)
+//     .then(registerSuccess)
+//     .catch(registerFail);
+
+//   function registerSuccess() {
+//     login.disableSubmit = false;
+//     login.login.username = login.register.username;
+//     login.login.password = login.register.password;
+//     submitLogin();
+//     clearRegisterForm();
+//   }
+
+//   function registerFail(error) {
+//     $log.error(error);
+//     login.disableSubmit = false;
+//     if (error.data.duplicateEmailAddress) {
+//       login.registerError = DUPLICATE_EMAIL_ERROR;
+//     } else {
+//       login.registerError = DEFAULT_REGISTRATION_ERROR;
+//     }
+//   }
+// }
+
+/**
+ * Submits the users request to reset the password.
+ */
+/* function submitForgotPassword() {
+  login.forgotPassword.recaptcha = vcRecaptchaService.getResponse(login.widgetId);
+  CustomerService.forgotPassword(login.forgotPassword)
+    .then(forgotPasswordSuccess)
+    .catch(forgotPasswordFail);
+
+  function forgotPasswordSuccess() {
+    login.disableSubmit = false;
+  }
+
+  function forgotPasswordFail() {
+    login.disableSubmit = false;
+    login.forgotPasswordError = 'There was an error submitting your request. Please try again.';
+  }
+
+  login.forgotPasswordResponse = "We have sent you an email with a link to reset your password.";
+} */
+
+/**
+ * Sets a boolean for the recaptcha being loaded.
+ */
+/* function setWidgetId(widgetId) {
+  widgetId = widgetId;
+  recaptchaLoaded = true;
+} */
+
+/**
+ * Toggles the forgot password view in the overlay
+ * on/off.
+ */
+/* function toggleForgotPassword() {
+  isLogin = false;
+  isForgotPassword = !isForgotPassword;
+} */
+
+/**
+ * Toggles the login/register views
+ */
+/* function toggleView() {
+  clearRegisterForm();
+  clearLoginForm();
+  clearForgotPasswordForm();
+  isLogin = !isLogin;
+  isForgotPassword = false;
+} */
+
+      /**
+       * Toggles the overlay opened/closed
+       */
+      /* function toggleOverlay() {
+          var directiveScope = OverlayService.get('login');
+          directiveScope.toggleOverlay();
+      } */
 }
