afa9652ac3d877da47017712443f57036a291bf7
Joshua Smith <smith.joshua@heb.com>
2017-10-03 11:25:13 -0500

    Added login/forgot password/register components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BannerComponent } from './banner-inline/banner-inline.component';
-import { LoginComponent } from './login/login.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import {AppComponent} from './app.component';
+import {BannerComponent} from './banner-inline/banner-inline.component';
+import {LoginComponent} from './login/login.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import {LoginFormComponent} from './login-form/login-form.component';
+import {RegisterFormComponent} from './register-form/register-form.component';
+import {ForgotPasswordFormComponent} from './forgot-password-form/forgot-password-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
     LoginComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    ForgotPasswordFormComponent
   ],
   imports: [
     BrowserModule
