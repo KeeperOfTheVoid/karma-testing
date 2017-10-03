625db500e3223ef5ffeda2a40f0441f898bab474
Matthew Lalmansingh <gpjazzplayer@gmail.com>
2017-10-03 15:16:26 -0500

    Refactor AngularJS to Angular
    - Refactor AngularJS to Angular syntax
    - Move login logic into component
    - Move register logic into component
    - Move forgot password logic into component
    - Implement login and register components

---
+++ src/app/register-form/model/register.ts
@@ -0,0 +1,12 @@
+export class Register {
+
+      constructor(
+        public emailAddress: string,
+        public emailConfirm: string,
+        public passwordConfirm: string,
+        public password: string,
+        public username: string,
+        public receiveEmail: boolean
+      ) {  }
+
+}
